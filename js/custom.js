798a8e708ef1203b1f29f72cba06df895cec96f5
Duong <nguyenthaiduong17082003@gmail.com>
2025-12-25 15:10:01 +0700

    hero 4

--- js/custom.js
+++ js/custom.js
@@ -1,13 +1,44 @@
 jQuery(document).ready(function () {
-    const swiper = new Swiper(".hero-domain.swiper", {
+    const swiper = new Swiper(".hero-domain__inner.swiper", {
         slidesPerView: 5,
-        spaceBetween: 16,
+        spaceBetween: 20,
+        speed: 1200,
         loop: true,
+        autoplay: {
+            delay: 4000,
+            disableOnInteraction: false,
+        },
         navigation: {
             nextEl: ".swiper-button-next",
             prevEl: ".swiper-button-prev",
         },
+
+        breakpoints: {
+            320: {
+                // mobile nhỏ
+                slidesPerView: 3,
+                spaceBetween: 10,
+            },
+            576: {
+                // mobile lớn
+                slidesPerView: 3,
+                spaceBetween: 15,
+            },
+            768: {
+                // tablet
+                slidesPerView: 3,
+                spaceBetween: 20,
+            },
+            1024: {
+                // laptop
+                slidesPerView: 4,
+                spaceBetween: 20,
+            },
+            1200: {
+                // desktop lớn
+                slidesPerView: 5,
+                spaceBetween: 20,
+            },
+        },
     });
 });
-
-console.log(1);
