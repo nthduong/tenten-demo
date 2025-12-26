be3d623b075fae801110d9f46ef2bc3a6c2c55fe
Duong <nguyenthaiduong17082003@gmail.com>
2025-12-26 17:51:05 +0700

    why-chose-3

--- js/custom.js
+++ js/custom.js
@@ -34,10 +34,31 @@ jQuery(document).ready(function () {
             .addClass("active");
     });
 
-    $(".why-choose-item").on("mouseenter", function () {
-        jQuery(this).closest(".why-choose__list").find(".why-choose-item").removeClass("current");
-        jQuery(this).addClass("current");
-    });
+    function bindWhyChooseEvents() {
+        $(".why-choose-item").off("mouseenter click");
+
+        if ($(window).width() > 992) {
+            $(".why-choose-item").on("mouseenter", function () {
+                $(".why-choose-item").removeClass("current");
+                $(this).addClass("current");
+            });
+        } else {
+            $(".why-choose-item").on("click", function () {
+                $(".why-choose-item").removeClass("current");
+                $(this).addClass("current");
+            });
+        }
+    }
+    bindWhyChooseEvents();
+
+    function checkMobileWhyChoose() {
+        if ($(window).width() <= 992) {
+            $(".why-choose-item").addClass("current");
+        } else {
+            $(".why-choose-item").removeClass("current");
+        }
+    }
+    checkMobileWhyChoose();
 
     //swiper
     const swiper = new Swiper(".hero-domain__inner.swiper", {
@@ -107,5 +128,66 @@ jQuery(document).ready(function () {
                 slidesPerView: 5,
             },
         },
+    });
+
+    let swiper3 = null;
+
+    function handleSwiper() {
+        const $general = $(".why-choose__general");
+        const $list = $(".why-choose__list");
+        const $item = $(".why-choose-item");
+        const isMobile = $(window).width() < 992;
+
+        if (isMobile && !swiper3) {
+            // Add class swiper
+            $general.addClass("swiper");
+            $list.addClass("swiper-wrapper");
+            $item.addClass("swiper-slide");
+
+            // Init Swiper
+            swiper3 = new Swiper(".why-choose__general.swiper", {
+                slidesPerView: 1,
+                spaceBetween: 12,
+                loop: true,
+                autoplay: {
+                    delay: 4000,
+                    disableOnInteraction: false,
+                },
+
+                pagination: {
+                    el: ".why-choose__general .swiper-pagination",
+                    clickable: true,
+                },
+
+                breakpoints: {
+                    320: {
+                        slidesPerView: 1,
+                        spaceBetween: 10,
+                    },
+
+                    767: {
+                        slidesPerView: 2,
+                        spaceBetween: 20,
+                    },
+                },
+            });
+        }
+
+        if (!isMobile && swiper3) {
+            swiper3.destroy(true, true);
+            swiper3 = null;
+
+            $general.removeClass("swiper");
+            $list.removeClass("swiper-wrapper");
+            $item.removeClass("swiper-slide");
+        }
+    }
+
+    handleSwiper();
+
+    $(window).on("resize", function () {
+        handleSwiper();
+        checkMobileWhyChoose();
+        bindWhyChooseEvents();
     });
 });
